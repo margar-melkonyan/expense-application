2a8a06a1ff32ec790a602483a9e97d6f6ee383bc
Mark <margar.melkonyan@bk.ru>
2024-07-10 19:10:15 +0300

    done with xlsx generation

---
+++ internal/consts/xlsx/cols.go
@@ -0,0 +1,11 @@
+package xlsx
+
+const (
+	A = 65
+	B = 66
+	D = 68
+	E = 69
+	F = 70
+	G = 71
+	H = 72
+)
--- internal/service/xlsx.go
+++ internal/service/xlsx.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"bytes"
+	"expense-application/internal/consts"
+	"expense-application/internal/consts/xlsx"
+	"expense-application/internal/model"
 	"expense-application/internal/repository"
 	"fmt"
 	"github.com/xuri/excelize/v2"
@@ -17,79 +20,186 @@ func NewXLSXService(budgetRepository repository.Budget) *XLSXService {
 	}
 }
 
-func (s XLSXService) GenDayReport(typeBudget string, userId int) *bytes.Buffer {
+func addTotalToXLSX(file *excelize.File, total float64, rowNumber int, stylesID []int) {
+	file.MergeCell(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.D)), rowNumber),
+		fmt.Sprintf("%s%d", string(rune(xlsx.E)), rowNumber),
+	)
+	file.SetCellStyle(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.D)), rowNumber),
+		fmt.Sprintf("%s%d", string(rune(xlsx.E)), rowNumber),
+		stylesID[0],
+	)
+	file.SetCellValue(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.D)), rowNumber),
+		"Category total :",
+	)
+
+	file.MergeCell(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.F)), rowNumber),
+		fmt.Sprintf("%s%d", string(rune(xlsx.G)), rowNumber),
+	)
+	file.SetCellStyle(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.F)), rowNumber),
+		fmt.Sprintf("%s%d", string(rune(xlsx.G)), rowNumber),
+		stylesID[1],
+	)
+	file.SetCellValue(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.F)), rowNumber),
+		fmt.Sprintf("%.2f", total),
+	)
+
+	file.SetCellStyle(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+		fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+		stylesID[0],
+	)
+	file.SetCellValue(
+		"Sheet1",
+		fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+		"руб.",
+	)
+}
+
+func genXLSX(header string, budgetCategories []model.Category) *bytes.Buffer {
 	file := excelize.NewFile()
 
-	headers := []string{"ID", "Имя", "Возраст"}
-	for i, header := range headers {
-		file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+i)), 1), header)
-	}
+	textStyle, _ := file.NewStyle(&excelize.Style{
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+			Vertical:   "center",
+		},
+	})
 
-	data := [][]interface{}{
-		{1, "John", 30},
-		{2, "Alex", 20},
-		{3, "Bob", 40},
-	}
+	dateFormat, _ := file.NewStyle(&excelize.Style{
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+			Vertical:   "center",
+		},
+		NumFmt: 15,
+	})
 
-	for i, row := range data {
-		dataRow := i + 2
-		for j, col := range row {
-			file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+j)), dataRow), col)
+	numberFormat, _ := file.NewStyle(&excelize.Style{
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+			Vertical:   "center",
+		},
+		NumFmt: 2,
+	})
+
+	file.MergeCell("Sheet1", "A1", "H1")
+	file.SetCellStyle("Sheet1", "A1", "H1", textStyle)
+	file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(xlsx.A)), 1), header)
+
+	rowNumber := 0
+	totalSum := 0.0
+
+	for _, category := range budgetCategories {
+		if len(category.Budgets) != 0 {
+			rowNumber += 3
+
+			file.MergeCell("Sheet1", fmt.Sprintf(
+				"%s%d",
+				string(rune(xlsx.A)), rowNumber),
+				fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+			)
+			file.SetCellStyle("Sheet1", fmt.Sprintf(
+				"%s%d",
+				string(rune(xlsx.A)), rowNumber),
+				fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+				textStyle,
+			)
+			file.SetCellValue("Sheet1", fmt.Sprintf(
+				"%s%d",
+				string(rune(xlsx.A)), rowNumber),
+				category.Name,
+			)
+
+			categorySum := 0.0
+			for j, budget := range category.Budgets {
+				rowNumber += 1
+
+				file.SetCellValue(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.A)), rowNumber),
+					j+1,
+				)
+
+				//Title Cell
+				file.SetCellValue(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.B)), rowNumber),
+					budget.Title,
+				)
+				file.MergeCell(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.B)), rowNumber),
+					fmt.Sprintf("%s%d", string(rune(xlsx.E)), rowNumber),
+				)
+
+				////Amount Cell
+				file.MergeCell(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.F)), rowNumber),
+					fmt.Sprintf("%s%d", string(rune(xlsx.G)), rowNumber),
+				)
+				file.SetCellStyle(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.F)), rowNumber),
+					fmt.Sprintf("%s%d", string(rune(xlsx.G)), rowNumber),
+					numberFormat,
+				)
+				file.SetCellValue(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.F)), rowNumber),
+					fmt.Sprintf("%.2f", budget.Amount/100),
+				)
+
+				// CreatedAt Cell
+				file.SetCellValue(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+					budget.CreatedAt,
+				)
+				file.SetCellStyle(
+					"Sheet1",
+					fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+					fmt.Sprintf("%s%d", string(rune(xlsx.H)), rowNumber),
+					dateFormat,
+				)
+
+				categorySum += budget.Amount / 100
+			}
+			rowNumber += 1
+			totalSum += categorySum
+			addTotalToXLSX(file, categorySum, rowNumber, []int{textStyle, numberFormat})
 		}
 	}
 
+	rowNumber += 4
+	addTotalToXLSX(file, totalSum, rowNumber, []int{textStyle, numberFormat})
 	buffer, _ := file.WriteToBuffer()
 
 	return buffer
 }
+
+func (s XLSXService) GenDayReport(typeBudget string, userId int) *bytes.Buffer {
+	budgets, _ := s.budgetRepository.GetBudgetByCategoryAndPeriod(typeBudget, userId, consts.Day)
+	return genXLSX(fmt.Sprintf("Current %s / %s", consts.Day, typeBudget), budgets)
+}
 func (s XLSXService) GenWeekReport(typeBudget string, userId int) *bytes.Buffer {
-	file := excelize.NewFile()
-
-	headers := []string{"ID", "Имя", "Возраст"}
-	for i, header := range headers {
-		file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+i)), 1), header)
-	}
-
-	data := [][]interface{}{
-		{1, "John", 30},
-		{2, "Alex", 20},
-		{3, "Bob", 40},
-	}
-
-	for i, row := range data {
-		dataRow := i + 2
-		for j, col := range row {
-			file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+j)), dataRow), col)
-		}
-	}
-
-	buffer, _ := file.WriteToBuffer()
-
-	return buffer
+	budgets, _ := s.budgetRepository.GetBudgetByCategoryAndPeriod(typeBudget, userId, consts.Week)
+	return genXLSX(fmt.Sprintf("Current %s / %s", consts.Week, typeBudget), budgets)
 }
 
 func (s XLSXService) GenMonthReport(typeBudget string, userId int) *bytes.Buffer {
-	file := excelize.NewFile()
-
-	headers := []string{"ID", "Имя", "Возраст"}
-	for i, header := range headers {
-		file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+i)), 1), header)
-	}
-
-	data := [][]interface{}{
-		{1, "John", 30},
-		{2, "Alex", 20},
-		{3, "Bob", 40},
-	}
-
-	for i, row := range data {
-		dataRow := i + 2
-		for j, col := range row {
-			file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+j)), dataRow), col)
-		}
-	}
-
-	buffer, _ := file.WriteToBuffer()
-
-	return buffer
+	budgets, _ := s.budgetRepository.GetBudgetByCategoryAndPeriod(typeBudget, userId, consts.Month)
+	return genXLSX(fmt.Sprintf("Current %s / %s", consts.Month, typeBudget), budgets)
 }
