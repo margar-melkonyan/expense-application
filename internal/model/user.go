74b38087931556c67f75d74bb785b94dabfeff9c
Mark <margar.melkonyan@bk.ru>
2024-07-25 13:20:47 +0300

    change user password validation

--- internal/model/user.go
+++ internal/model/user.go
@@ -10,7 +10,7 @@ type User struct {
 	TgId      int64          `json:"tg_id,omitempty" gorm:"tg_id:bigint;not null" binding:"numeric,gt=0"`
 	Name      string         `json:"name" gorm:"name:varchar(255);not null" binding:"required,alphanum,min=2,max=255"`
 	Email     string         `json:"email" gorm:"email:varchar(255);not null" binding:"required,email"`
-	Password  string         `json:"password" gorm:"password:varchar(255);not null" binding:"required,alphanum"`
+	Password  string         `json:"password" gorm:"password:varchar(255);not null" binding:"required,ascii,gt=7"`
 	CreatedAt time.Time      `json:"-" gorm:"created_at:timestamp;not null"`
 	UpdatedAt time.Time      `json:"-" gorm:"updated_at:timestamp;not null"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"deleted_at:timestamp;default:null"`
@@ -19,7 +19,7 @@ type User struct {
 
 	// fields only for request
 	RefreshToken         []byte `json:"refresh_token,omitempty" gorm:"refresh_token:jsonb;default:null"`
-	PasswordConfirmation string `json:"password_confirmation" gorm:"-" binding:"required,alphanum,eqfield=password"`
+	PasswordConfirmation string `json:"password_confirmation" gorm:"-" binding:"required,ascii,gt=7,eqfield=password"`
 }
 
 type UserResponse struct {
