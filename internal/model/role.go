01d63f9a88c299e83e775b8539543e9ab2218070
Mark <margar.melkonyan@bk.ru>
2024-07-25 13:18:55 +0300

    added validation

--- internal/model/role.go
+++ internal/model/role.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Role struct {
-	Id                      uint           `json:"id" gorm:"primary_key;auto_increment"`
-	Title                   string         `json:"title" gorm:"varchar(100)"`
-	DisplayTitle            string         `json:"display_title" gorm:"varchar(100)"`
+	Id                      uint           `json:"id" gorm:"primary_key;auto_increment" binding:"numeric,min=1"`
+	Title                   string         `json:"title" gorm:"varchar(100)" binding:"required,alpha,min=1,max=255"`
+	DisplayTitle            string         `json:"display_title" gorm:"varchar(100)" binding:"required,min=1,max=255"`
 	Permissions             []byte         `json:"-" gorm:"jsonb"`
-	PermissionsUnmarshalled []string       `json:"permissions" gorm:"-"`
+	PermissionsUnmarshalled []string       `json:"permissions" gorm:"-" binding:"required,dive,alpha,min=1,max=255"`
 	CreatedAt               time.Time      `json:"-" gorm:"created_at:timestamp;not null"`
 	UpdatedAt               time.Time      `json:"-" gorm:"updated_at:timestamp;not null"`
 	DeletedAt               gorm.DeletedAt `json:"-" gorm:"deleted_at:timestamp;default:null"`
--- internal/model/user_roles.go
+++ internal/model/user_roles.go
@@ -2,7 +2,7 @@ package model
 
 type UserRole struct {
 	UserID uint `json:"-" gorm:"primary_key"`
-	RoleID uint `json:"role_id,omitempty" gorm:"primary_key"`
+	RoleID uint `json:"role_id,omitempty" gorm:"primary_key" binding:"required,numeric,gt=0"`
 	User   User `gorm:"foreignkey:UserID"`
 	Role   Role `gorm:"foreignkey:RoleID"`
 }
